eaa8a31cdd18680058d9c38635c8174d54822e93
mateo <matteoramirezz23@gmail.com>
2023-08-05 19:31:32 -0300

    creacion de lazyLoad home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeModule } from './modules/home/home.module';
 
 const routes: Routes = [
   //lazyloading
   {
     path: 'auth',
     loadChildren:()=>import('./modules/auth/auth.module').then(m=>m.AuthModule),
+  },
+  {
+    path:'home',
+    loadChildren:()=>import('./modules/home/home.module').then(m=>m.HomeModule)
   }
 ];
 
