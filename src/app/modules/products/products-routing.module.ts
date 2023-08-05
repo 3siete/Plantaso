58bc2a290a198eaa28839c1e4da296617b3e9934
mateo <matteoramirezz23@gmail.com>
2023-08-05 20:10:07 -0300

    feat: creacion de ruta secundaria products

--- src/app/modules/products/products-routing.module.ts
+++ src/app/modules/products/products-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductsPageComponent } from './pages/products-page/products-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //ruta secundaria
+  {path:'products',component:ProductsPageComponent}
+  
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
