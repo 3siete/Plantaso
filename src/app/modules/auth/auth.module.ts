4b3a4b1eed8df0b1652a3a4c70d66fd43a094925
mateo <matteoramirezz23@gmail.com>
2023-08-05 17:41:12 -0300

    feat: creacion de page login

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LoginComponent } from './pages/login/login.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent
+  ],
   imports: [
     CommonModule
   ]
