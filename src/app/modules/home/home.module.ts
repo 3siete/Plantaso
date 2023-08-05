ac2898510b24959569806d1cdb128fe4a4607ddd
mateo <matteoramirezz23@gmail.com>
2023-08-05 20:05:21 -0300

    feat: creacion de home-page

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomePageComponent
+  ],
   imports: [
     CommonModule,
     HomeRoutingModule
