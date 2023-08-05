f5976b3a29bcf786f5b28197f79c829d5072d241
mateo <matteoramirezz23@gmail.com>
2023-08-05 20:06:29 -0300

    feat: creacion de ruta secundaria de home

--- src/app/modules/home/home-routing.module.ts
+++ src/app/modules/home/home-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home',component:HomePageComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
