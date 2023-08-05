6eee369860b3dc6ec94340d54289472f650f5165
mateo <matteoramirezz23@gmail.com>
2023-08-05 20:07:47 -0300

    feat: creacion de products-page

--- src/app/modules/products/products.module.ts
+++ src/app/modules/products/products.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ProductsRoutingModule } from './products-routing.module';
+import { ProductsPageComponent } from './pages/products-page/products-page.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ProductsPageComponent
+  ],
   imports: [
     CommonModule,
     ProductsRoutingModule
