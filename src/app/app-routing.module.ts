e60a5508c9f447cf1aa9ecac4da0f42e4b70479b
mateo <matteoramirezz23@gmail.com>
2023-08-05 19:22:04 -0300

    feat: creacion de lazyLoad auth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //lazyloading
+  {
+    path: 'auth',
+    loadChildren:()=>import('./modules/auth/auth.module').then(m=>m.AuthModule),
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
