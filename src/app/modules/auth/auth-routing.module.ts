f963c0485ac3a7c158b4ceca3346f5e8cf6d17b1
mateo <matteoramirezz23@gmail.com>
2023-08-05 19:25:03 -0300

    feat: creacion de ruta secundaria register

--- src/app/modules/auth/auth-routing.module.ts
+++ src/app/modules/auth/auth-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 const routes: Routes = [
 //rutas secundarias.
   {path:'login',component:LoginComponent},
+  {path:'register',component:RegisterComponent}
 ];
 
 @NgModule({
