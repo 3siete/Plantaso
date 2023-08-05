4dfbf863bfe9d16fd528c650ba38d66fa45fdaaf
mateo <matteoramirezz23@gmail.com>
2023-08-05 20:03:57 -0300

    feat: creacion de ruta secundaria articles

--- src/app/modules/articles/articles-routing.module.ts
+++ src/app/modules/articles/articles-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ArticlesPageComponent } from './pages/articles-page/articles-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //ruta secundaria
+  {path:'articulos',component:ArticlesPageComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
