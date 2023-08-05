4cc10236186b39f784e1dd8a33ff27739092d41a
mateo <matteoramirezz23@gmail.com>
2023-08-05 19:37:06 -0300

    feat: creacion de routing-module articles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeModule } from './modules/home/home.module';
 
 const routes: Routes = [
   //lazyloading
--- src/app/modules/articles/articles.module.ts
+++ src/app/modules/articles/articles.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { ArticlesRoutingModule } from './articles-routing.module';
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    ArticlesRoutingModule
   ]
 })
 export class ArticlesModule { }
