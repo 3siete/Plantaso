0468cf0f214c3279c44261ad712a757ea6f04032
MateoGs013 <mateogabus@gmail.com>
2023-08-22 08:52:25 -0300

    feat(Contenedor): creando componente contenedor
    este componente se va a utilizar en la home page  mas de una vez, por eso su componente correspondiente

---
+++ src/app/modules/home/components/contenedor/contenedor.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ContenedorComponent } from './contenedor.component';
+
+describe('ContenedorComponent', () => {
+  let component: ContenedorComponent;
+  let fixture: ComponentFixture<ContenedorComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ContenedorComponent]
+    });
+    fixture = TestBed.createComponent(ContenedorComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/modules/home/components/contenedor/contenedor.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contenedor',
+  templateUrl: './contenedor.component.html',
+  styleUrls: ['./contenedor.component.css']
+})
+export class ContenedorComponent {
+
+}
