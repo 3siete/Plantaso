d08f4dd20931393e662b69f974e8dbe293ce3c58
MateoGs013 <mateogabus@gmail.com>
2023-08-31 14:10:03 -0300

    feat(Cards): creacion de componente card-shop
    este comopnente lo vamos a utilizar para realizar la tarjeta que se vaya a utilizzar para los productos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { CardShopComponent } from './app/shared/components/card-shop/card-shop/card-shop.component';
 @NgModule({
   declarations: [
     AppComponent,
+    CardShopComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { AppRoutingModule } from '../app-routing.module';
+import { CardShopComponent } from './components/card-shop/card-shop.component';
 
 
 @NgModule({
   declarations: [
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    CardShopComponent
   ],
   imports: [
     CommonModule,
