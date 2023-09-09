d7fd6bdaf7da3cdce97c56865a9847d2c82da169
mateo <matteoramirezz23@gmail.com>
2023-09-08 23:21:08 -0300

    feat(sidebar): creacion del componente sidebar

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './components/footer/footer.component';
 import { ContainerComponent } from './components/container/container.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 
 @NgModule({
   declarations: [
     FooterComponent,
-    ContainerComponent
+    ContainerComponent,
+    SidebarComponent
     
   ],
   imports: [
