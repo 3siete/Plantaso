318d66599e637f425b74029ea730c124d179bc8d
mateo <matteoramirezz23@gmail.com>
2023-09-10 19:57:28 -0300

    refactor(sidebar): correccion de nombre de variable

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent {
-true:boolean = false;
+hover:boolean = false;
 
   mouseover(){
     if(hover:true){
