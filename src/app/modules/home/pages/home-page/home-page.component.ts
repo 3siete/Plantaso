4f3079179ad4fc0f8bb0f39ab122b7afd6992550
MateoGs013 <mateogabus@gmail.com>
2023-09-16 18:27:43 -0300

    feat(home-page/ts): importaciones necesarias x2

---
+++ src/app/modules/home/pages/home-page/home-page.component.ts
@@ -0,0 +1,12 @@
+// Importaciones necesarias
+import { Component, OnInit, ElementRef } from '@angular/core';
+import { gsap } from "gsap";
+
+@Component({
+  selector: 'app-home-page',
+  templateUrl: './home-page.component.html',
+  styleUrls: ['./home-page.component.css']
+})
+export class HomePageComponent {
+
+}
