b4bea295235fb32848aa786cb3101aee2fadc66e
MateoGs013 <mateogabus@gmail.com>
2023-09-16 21:32:16 -0300

    feat(carrusel): importando el modulo de carrusel

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CarouselModule } from 'primeng/carousel';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomePageComponent } from './pages/home-page/home-page.component';
