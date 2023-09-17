50d647ea690b764662b5fc20dcee387d0a19d55c
MateoGs013 <mateogabus@gmail.com>
2023-09-16 21:23:31 -0300

    feat: creacion del componente carrusel

---
+++ src/app/modules/home/components/carrusel/carrusel.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carrusel',
+  templateUrl: './carrusel.component.html',
+  styleUrls: ['./carrusel.component.css']
+})
+export class CarruselComponent {
+
+}
--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { CarruselComponent } from './components/carrusel/carrusel.component';
 
 
 @NgModule({
   declarations: [
-    HomePageComponent
+    HomePageComponent,
+    CarruselComponent
   ],
   imports: [
     CommonModule,
