a7e6db72fd58ec40d24a1793352590a029f9a2f4
mateo <matteoramirezz23@gmail.com>
2023-09-19 00:49:10 -0300

    fix(auth-register): importardo inject de angular/core

--- src/app/modules/auth/pages/register/register.component.ts
+++ src/app/modules/auth/pages/register/register.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { User } from '../../models/user.model';
 import { FormBuilder } from '@angular/forms';
 
