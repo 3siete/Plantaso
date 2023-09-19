f5f920a60c543b1ca80a030ffafeb7ca8c9afeb4
mateo <matteoramirezz23@gmail.com>
2023-09-19 16:43:59 -0300

    refactor(auth-register): cambios en los formGroup y formBuilder

--- src/app/modules/auth/pages/register/register.component.ts
+++ src/app/modules/auth/pages/register/register.component.ts
@@ -2,12 +2,8 @@ import { Component, inject } from '@angular/core';
 import { User } from '../../models/user.model';
 import { FormBuilder, FormControl, FormGroup, Validators, ReactiveFormsModule} from '@angular/forms';
 
-interface SignUpForm {
-  names: FormControl<string>;
-  lastName: FormControl<string>;
-  email: FormControl<string>;
-  password: FormControl<string>;
-}
+
+  
 
 @Component({
   selector: 'app-register',
@@ -17,28 +13,21 @@ interface SignUpForm {
 export class RegisterComponent {
   
   formBuilder = inject(FormBuilder)
+  registerForm!: FormGroup;
 
-  form: FormGroup<SignUpForm> = this.formBuilder.group({
-    names: this.formBuilder.control('', {
-      validators: Validators.required,
-      nonNullable: true,
-    }),
-    lastName: this.formBuilder.control('', {
-      validators: Validators.required,
-      nonNullable: true,
-    }),
-    email: this.formBuilder.control('', {
-      validators: [Validators.required, Validators.email],
-      nonNullable: true,
-    }),
-    password: this.formBuilder.control('', {
-      validators: Validators.required,
-      nonNullable: true,
-    }),
-  });
+  ngOnInit(): void {
+    this.registerForm = this.formBuilder.group({
+      name : ['',[Validators.required] ],
+      lastname : ['', [Validators.required]],
+      email : ['',[Validators.required, Validators.email]],
+      passoword : ['', [Validators.required, Validators.minLength(7)]]
+    })
+  }
+
+
 
   onSubmit(){
-    if (this.form.valid) return;
-    console.log(this.form.value)    
+    if (this.registerForm.valid) return;
+    console.log(this.registerForm.value)    
   } 
 }
