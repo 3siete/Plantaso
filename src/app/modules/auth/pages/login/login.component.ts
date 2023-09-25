a2848c4dc8647c849c484c113d63b2a8ba824679
mateo <matteoramirezz23@gmail.com>
2023-09-24 21:13:44 -0300

    feat(auth-login): import de formBuilder

--- src/app/modules/auth/pages/login/login.component.ts
+++ src/app/modules/auth/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  constructor(private fb:FormBuilder){}
 
 }
