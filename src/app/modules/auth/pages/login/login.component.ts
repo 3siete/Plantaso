84c7150d05cdf03159b5c0db701d86415dd6edf1
mateo <matteoramirezz23@gmail.com>
2023-09-24 21:14:44 -0300

    feat(auth-login): declarando loginForm como un form group

--- src/app/modules/auth/pages/login/login.component.ts
+++ src/app/modules/auth/pages/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -8,5 +8,6 @@ import { FormBuilder } from '@angular/forms';
 })
 export class LoginComponent {
   constructor(private fb:FormBuilder){}
+  loginForm!:FormGroup;
 
 }
