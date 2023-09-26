39f916508143d54cbfe80e8254abfa531d0100da
mateo <matteoramirezz23@gmail.com>
2023-09-26 17:50:01 -0300

    feat(auth): importando Subject

--- src/app/modules/auth/services/auth.service.ts
+++ src/app/modules/auth/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth'
 import { AngularFirestore } from '@angular/fire/compat/firestore'
+import { Subject } from 'rxjs';
 
 
 @Injectable({
