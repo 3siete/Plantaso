221d36e4b5019ddf3f518dc7672b53bf6b0eee7c
mateo <matteoramirezz23@gmail.com>
2023-09-26 17:51:07 -0300

    feat(auth): importando interfaz de usuario

--- src/app/modules/auth/services/auth.service.ts
+++ src/app/modules/auth/services/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth'
 import { AngularFirestore } from '@angular/fire/compat/firestore'
 import { Subject } from 'rxjs';
+import { User } from '../models/user.model';
 
 
 @Injectable({
