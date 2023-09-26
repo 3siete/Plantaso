695322d5ab67c5add107ff633e1dd0d9d007facb
mateo <matteoramirezz23@gmail.com>
2023-09-26 19:18:16 -0300

    feat(auth): creacion de userRef y userData para guardar la informacion del usuario en firestore

--- src/app/modules/auth/models/user.model.ts
+++ src/app/modules/auth/models/user.model.ts
@@ -3,6 +3,6 @@ export interface User{
     uid:string | any;
     name:string;
     lastname:string;
-    mail:string;
+    email:string;
     password:string; //signo de interrogación para no guardar la contraseña --- para modificar en el futuro
 }
--- src/app/modules/auth/services/auth.service.ts
+++ src/app/modules/auth/services/auth.service.ts
@@ -33,13 +33,25 @@ export class AuthService {
 
   register(user: any): Promise<any> {
     return this.afAuth.createUserWithEmailAndPassword(user.email, user.password).then((res) => {
-      this.setUserData(res.user, user.name, user.password)
+      this.setUserData(res.user, user.name)
     })
   }
 
   setUserData(user:any, name?:string): Promise<any> {
     const userRef: AngularFirestoreDocument<any> = this.afs.doc(`users/${user.uid}`)
-    
+    const userData:User = {
+      uid:user.uid,
+      name:user.name,
+      lastname:user.lastname,
+      email:user.email,
+      password:user.password,
+    }
+    return userRef.set(userData, {
+      merge: true
+    })
+    }
+  
+
   }
 
 
