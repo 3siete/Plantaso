a479c1781e266eb56b4d74cf9f6e6fb3d1599c4e
mateo <matteoramirezz23@gmail.com>
2023-09-26 17:42:07 -0300

    feat(auth): importando angular fire store

--- src/app/modules/auth/services/auth.service.ts
+++ src/app/modules/auth/services/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth'
+import { AngularFirestore } from '@angular/fire/compat/firestore'
 
 
 @Injectable({
