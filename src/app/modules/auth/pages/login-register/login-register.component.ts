d652a5b08b1fe27c73920a61be49459e4d1c4180
MateoGs013 <mateogabus@gmail.com>
2023-09-27 23:45:38 -0300

    feat(login-ts): Obtener una referencia al elemento con el id 'sign-in-btn' en el HTML

--- src/app/modules/auth/pages/login-register/login-register.component.ts
+++ src/app/modules/auth/pages/login-register/login-register.component.ts
@@ -8,5 +8,7 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 export class LoginRegisterComponent {
   // Obtener una referencia al elemento con la clase 'container' en el HTML
   @ViewChild('container') container!: ElementRef;
+  // Obtener una referencia al elemento con el id 'sign-in-btn' en el HTML
+  @ViewChild('sign_in_btn') signInBtn!: ElementRef;
  
 }
