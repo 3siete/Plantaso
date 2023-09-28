1658c2c289335fe657505e5aabfeef4a11d412a1
MateoGs013 <mateogabus@gmail.com>
2023-09-27 23:35:18 -0300

    feat(login-ts): Obtener una referencia al elemento con la clase 'container' en el HTML

--- src/app/modules/auth/pages/login-register/login-register.component.ts
+++ src/app/modules/auth/pages/login-register/login-register.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-login-register',
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./login-register.component.css']
 })
 export class LoginRegisterComponent {
-
+  // Obtener una referencia al elemento con la clase 'container' en el HTML
+  @ViewChild('container') container!: ElementRef;
+ 
 }
