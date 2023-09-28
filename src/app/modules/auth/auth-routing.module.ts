1778ecc8b5e82adfcd504cc1a8552dad9b105bc3
mateo <matteoramirezz23@gmail.com>
2023-09-28 15:55:05 -0300

    feat(login-register): creando ruta

--- src/app/modules/auth/auth-routing.module.ts
+++ src/app/modules/auth/auth-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { LoginRegisterComponent } from './pages/login-register/login-register.component';
 
 const routes: Routes = [
 //rutas secundarias.
   {path:'iniciarsesion',component:LoginComponent},
-  {path:'registrarte',component:RegisterComponent}
+  {path:'registrarte',component:RegisterComponent},
+  {path: 'login-register', component: LoginRegisterComponent}
 ];
 
 @NgModule({
