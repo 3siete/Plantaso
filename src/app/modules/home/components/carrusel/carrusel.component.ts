92be6d2586bee122dab6c29b8c33d71738a7d1f6
mateo <matteoramirezz23@gmail.com>
2023-10-14 23:00:51 -0300

    refactor(carrucel): cambios en el componente

--- src/app/modules/home/components/carrusel/carrusel.component.ts
+++ src/app/modules/home/components/carrusel/carrusel.component.ts
@@ -1,18 +1,8 @@
 import { Component } from '@angular/core';
-import { CardPostComponent } from 'src/app/shared/components/card-post/card-post.component';
-
 @Component({
   selector: 'app-carrusel',
-  templateUrl: './carrusel.component.html',
-  styleUrls: ['./carrusel.component.css']
+  template: ``,
 })
 export class CarruselComponent {
-  cards = [
-    {
-      title:'',
-      subtitle:'',
-      description:'',
-      img:''
-    }
-  ]
+  
 }
