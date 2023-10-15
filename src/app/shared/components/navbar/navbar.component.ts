20178a5e3b0e9cc8e91f9681ed54694245197d0e
MateoGs013 <mateogabus@gmail.com>
2023-10-14 22:36:45 -0300

    feat(navbar-ts): Decorador @component que define las propiedades del componente

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,6 +1,11 @@
 // Importando módulos necesarios de Angular
 import { Component, ElementRef, AfterViewInit } from '@angular/core';
 
-
+// Decorador @Component que define las propiedades del componente
+@Component({
+  selector: 'app-navbar', // Selector CSS para usar este componente
+  templateUrl: './navbar.component.html', // Ruta al archivo HTML del componente
+  styleUrls: ['./navbar.component.css'] // Ruta al archivo CSS del componente
+})
 
 
