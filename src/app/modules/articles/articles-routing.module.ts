03f461e6820985911d9d5dbac8709b9f60508863
MateoGs013 <mateogabus@gmail.com>
2023-10-31 15:59:46 -0300

    feat: Configurando la ruta para la vista del articulo

--- src/app/modules/articles/articles-routing.module.ts
+++ src/app/modules/articles/articles-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ArticlesPageComponent } from './pages/articles-page/articles-page.component';
+import { ArticlePageComponent } from './pages/article-page/article-page.component';
 
 const routes: Routes = [
   //ruta secundaria
-  {path:'',component:ArticlesPageComponent},
+  { path: '', component: ArticlesPageComponent },
+  { path: '/article', component: ArticlePageComponent }
 ];
 
 @NgModule({
