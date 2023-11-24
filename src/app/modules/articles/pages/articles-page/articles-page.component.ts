a9bb5d45a0b7ae215ed74857bcccddd5c11dcb85
mateo <matteoramirezz23@gmail.com>
2023-11-24 15:24:04 -0300

    feat(articles-page): importando el crudSevice

--- src/app/modules/articles/pages/articles-page/articles-page.component.ts
+++ src/app/modules/articles/pages/articles-page/articles-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CardPost } from 'src/app/models/card-post';
+import { CrudService } from 'src/app/modules/admin/services/crud.service';
 
 @Component({
   selector: 'app-articles-page',
