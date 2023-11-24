05334253737d17876ad8c20b7fc4c51286e72b75
mateo <matteoramirezz23@gmail.com>
2023-11-24 14:50:03 -0300

    feat(crudservice): injectando angularfirestore

--- src/app/modules/admin/services/crud.service.ts
+++ src/app/modules/admin/services/crud.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CrudService {
 
-  constructor() { }
+  constructor(private ads: AngularFirestore) { }
 }
