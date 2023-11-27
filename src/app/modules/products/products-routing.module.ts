5a3c28c54867e29ca4b83f4214e3c7e7bb9990aa
MateoGs013 <mateogabus@gmail.com>
2023-11-27 16:10:06 -0300

    chore(product-page): cambios en estilos

--- src/app/modules/products/products-routing.module.ts
+++ src/app/modules/products/products-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductsPageComponent } from './pages/products-page/products-page.component';
+import { ProductPageComponent } from './pages/product-page/product-page.component';
 
 const routes: Routes = [
   //ruta secundaria
-  {path:'',component:ProductsPageComponent}
+  {path:'',component:ProductsPageComponent},
+  {path:'producto',component:ProductPageComponent}
   
 ];
 
