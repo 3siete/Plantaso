643d7c64ed6433cf4f55d6dccb6909754ef4ac80
mateo <matteoramirezz23@gmail.com>
2023-11-28 01:50:57 -0300

    feat(create-article-button): creando formulario con validadores en el OnInit

--- src/app/modules/articles/admin/components/create-article-buttom/create-article-button.component.ts
+++ src/app/modules/articles/admin/components/create-article-buttom/create-article-button.component.ts
@@ -9,9 +9,23 @@ import { FormBuilder } from '@angular/forms';
 export class CreateArticleButtomComponent {
   visible: boolean = false;
 
+  createForm!: FormGroup;
   constructor(private fb:FormBuilder){}
 
   ngOnInit():void{
+    this.createForm = this.fb.group({
+      imgURL: ['', [Validators.required,]],
+      alt: ['', [Validators.required]],
+      title: ['', [Validators.required]],
+      subtitle: ['', [Validators.required]],
+      description: ['', [Validators.required]],
+      reproduction: ['', [Validators.required]],
+      care: ['', [Validators.required]],
+      tips: ['', [Validators.required]],
+      characteristic: ['', [Validators.required]],
+      pests: ['', [Validators.required]],
+      insecticides: ['', [Validators.required]],
+    });
 
   }
   showDialog() {
