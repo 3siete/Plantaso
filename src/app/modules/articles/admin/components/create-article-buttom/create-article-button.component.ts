c8f3b6b3dab928bf63a08d0422fc610ea1429f1d
mateo <matteoramirezz23@gmail.com>
2023-11-28 01:51:22 -0300

    feat(create-article-button): importando FormGroup

--- src/app/modules/articles/admin/components/create-article-buttom/create-article-button.component.ts
+++ src/app/modules/articles/admin/components/create-article-buttom/create-article-button.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-create-article-button',
