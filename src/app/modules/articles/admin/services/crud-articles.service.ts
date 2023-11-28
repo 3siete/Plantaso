a8886f2f38cc44df8d8c1e5ef7ea2158ed217d24
mateo <matteoramirezz23@gmail.com>
2023-11-28 01:08:22 -0300

    feat(crud-articles): importando angularFirestoreCollection

--- src/app/modules/articles/admin/services/crud-articles.service.ts
+++ src/app/modules/articles/admin/services/crud-articles.service.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
+import { Article } from 'src/app/models/articles.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CrudArticlesService {
 
-  constructor(private afs: AngularFirestore) { }
+  constructor(
+    private afs: AngularFirestore,
+    private articlesCollection: AngularFirestoreCollection<Article>) { }
 
 
 
