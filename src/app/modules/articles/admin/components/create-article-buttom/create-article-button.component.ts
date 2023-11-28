2dadfeb6663e13269b6dc15bfd6a8a03047e03e8
mateo <matteoramirezz23@gmail.com>
2023-11-28 01:51:40 -0300

    feat(create-article-button): importando Validadores

--- src/app/modules/articles/admin/components/create-article-buttom/create-article-button.component.ts
+++ src/app/modules/articles/admin/components/create-article-buttom/create-article-button.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-article-button',
