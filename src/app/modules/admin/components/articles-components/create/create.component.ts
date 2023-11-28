8cce542c1aad06c21e3a5b350eec90e4ec3beae7
mateo <matteoramirezz23@gmail.com>
2023-11-27 21:49:58 -0300

    feat(create-article): injectando formbuilder

--- src/app/modules/admin/components/articles-components/create/create.component.ts
+++ src/app/modules/admin/components/articles-components/create/create.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-create',
@@ -9,7 +10,7 @@ export class CreateComponent {
   visible: boolean = false;
 
 
-  constructor(){}
+  constructor(private fb:FormBuilder){}
   
   
   showDialog() {
