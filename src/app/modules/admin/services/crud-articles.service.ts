2c4429e074079e4eec8bbdd0a795aaf43220e1ea
mateo <matteoramirezz23@gmail.com>
2023-11-29 13:16:13 -0300

    feat(crud-articles-services): creando metodo createArticle

--- src/app/modules/admin/services/crud-articles.service.ts
+++ src/app/modules/admin/services/crud-articles.service.ts
@@ -9,4 +9,24 @@ export class CrudArticlesService {
   constructor(
     private afs: AngularFirestore,
     private articlesCollection: AngularFirestoreCollection) { }
+
+
+
+     //CREATE
+  createArticle(article: Article): Observable<void> {
+    const idArticle = this.afs.createId();
+    article.id = idArticle;
+
+    return from(this.articlesCollection.add(article)).pipe(
+      switchMap((docRef: DocumentReference<Article>) => {
+        // Actualizar el artículo con el ID asignado por Firestore
+        return from(docRef.set({ ...article, id: docRef.id }));
+      }),
+      catchError((error) => {
+        console.error('Error desconocido al crear el artículo', error);
+        return throwError(error);
+      })
+    );
+  }
 }
+
