14c0959a5e4da679e87da4f0b68d598a600c2eaa
mateo <matteoramirezz23@gmail.com>
2023-11-28 21:48:15 -0300

    fix(admin): eliminando importaciones en adminModule

--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -2,9 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
-import { CreateComponent } from './components/articles-components/create/create.component';
-import { UpdateComponent } from './components/articles-components/update/update.component';
-import { DeleteComponent } from './components/articles-components/delete/delete.component';
 
 //primeng
 import { DialogModule } from 'primeng/dialog';
