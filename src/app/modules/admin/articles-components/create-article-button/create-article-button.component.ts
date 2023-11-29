35e1b4e9bae80162aef39f7b5681ba25f50ac505
mateo <matteoramirezz23@gmail.com>
2023-11-29 13:10:00 -0300

    feat(create-article-button): importando validadores

--- src/app/modules/admin/articles-components/create-article-button/create-article-button.component.ts
+++ src/app/modules/admin/articles-components/create-article-button/create-article-button.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CrudArticlesService } from '../../services/crud-articles.service';
 
 @Component({
