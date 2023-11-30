51f226c8d77354cc90b1c886fe69c55058b75586
mateo <matteoramirezz23@gmail.com>
2023-11-30 15:20:18 -0300

    docs(admin): comentando

--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -12,6 +12,7 @@ import { DialogModule } from 'primeng/dialog';
 import { CreateArticleButtonComponent } from './articles-components/create-article-button/create-article-button.component';
 import { UpdateArticleButtonComponent } from './articles-components/update-article-button/update-article-button.component';
 import { DeleteArticleButtonComponent } from './articles-components/delete-article-button/delete-article-button.component';
+//PRODUCTOS
 import { CreateProductButtonComponent } from './products-components/create-product-button/create-product-button.component';
 import { UpdateProductButtonComponent } from './products-components/update-product-button/update-product-button.component';
 import { DeleteProductButtonComponent } from './products-components/delete-product-button/delete-product-button.component';
@@ -23,6 +24,8 @@ import { DeleteProductButtonComponent } from './products-components/delete-produ
   CreateArticleButtonComponent,
   UpdateArticleButtonComponent,
   DeleteArticleButtonComponent,
+
+  //PRODUCTOS
   CreateProductButtonComponent,
   UpdateProductButtonComponent,
   DeleteProductButtonComponent
