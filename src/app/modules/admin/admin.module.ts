85cc352e3ea693a2ed9550ffc307d20efab7bc17
mateo <matteoramirezz23@gmail.com>
2023-11-30 15:19:46 -0300

    docs(admin): mas comentarios

--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -8,7 +8,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SpeedDialModule } from 'primeng/speeddial';
 import { DialogModule } from 'primeng/dialog';
 
-
+//ARTICULOS
 import { CreateArticleButtonComponent } from './articles-components/create-article-button/create-article-button.component';
 import { UpdateArticleButtonComponent } from './articles-components/update-article-button/update-article-button.component';
 import { DeleteArticleButtonComponent } from './articles-components/delete-article-button/delete-article-button.component';
