c715622e782a3d3e2a4aa9036728771330f586b9
mateo <matteoramirezz23@gmail.com>
2023-11-30 18:39:18 -0300

    feat(article-page): logica para llamar los datos

--- src/app/modules/articles/pages/article-page/article-page.component.ts
+++ src/app/modules/articles/pages/article-page/article-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CrudArticlesService } from 'src/app/modules/admin/services/crud-articles.service';
 
 @Component({
   selector: 'app-article-page',
@@ -6,5 +8,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./article-page.component.css']
 })
 export class ArticlePageComponent {
+  article: any; // Ajusta el tipo según tu modelo de datos
 
+  constructor(
+    private route: ActivatedRoute,
+    private articleService: CrudArticlesService // Asume que tienes un servicio para manejar los datos de los artículos
+  ) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      const articleSlug = params.get('nombreArticulo');
+      this.loadArticle(articleSlug);
+    });
+  }
+
+  private loadArticle(slug: string | null): void {
+    if (slug) {
+      // Aquí, realiza la lógica para cargar el artículo basado en el slug
+      // Por ejemplo, puedes llamar a un servicio que haga una solicitud HTTP para obtener los datos del artículo
+      this.articleService.getArticleBySlug(slug).subscribe(
+        data => {
+          this.article = data;
+        },
+        error => {
+          console.error('Error al cargar el artículo', error);
+        }
+      );
+    }
+  }
 }
