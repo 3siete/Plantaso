882c870e50e006d2e0d8f0887becb35a817da06b
mateo <matteoramirezz23@gmail.com>
2023-11-30 16:07:29 -0300

    feat(delete-article-button): creando boton delete en html

--- src/app/modules/admin/articles-components/delete-article-button/delete-article-button.component.ts
+++ src/app/modules/admin/articles-components/delete-article-button/delete-article-button.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-delete-article-button',
-  template: ``,
+  template: `<i class="fa-solid fa-trash fa-2xl" style="color: #ffffff;"></i>`,
   styleUrls: ['./delete-article-button.component.css']
 })
 export class DeleteArticleButtonComponent {
