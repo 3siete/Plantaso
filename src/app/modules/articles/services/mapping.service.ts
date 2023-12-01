c1e8c5220c67cd0ef9a1885d87b6b3e8b0163a8b
mateo <matteoramirezz23@gmail.com>
2023-12-01 06:03:40 -0300

    feat(mapping Service): añadiendo metodo para obtener id

--- src/app/modules/articles/services/mapping.service.ts
+++ src/app/modules/articles/services/mapping.service.ts
@@ -5,11 +5,14 @@ import { Injectable } from '@angular/core';
 })
 export class MappingService {
   private slugToIdMap: { [slug: string]: string } = {};
+  
   addMapping(slug: string, articleId: string): void {
     this.slugToIdMap[slug] = articleId;
   }
 
-
+  getArticleIdFromSlug(slug: string): string | null {
+    return this.slugToIdMap[slug] || null;
+  }
 
 
 }
