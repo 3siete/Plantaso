cd121a07af3238192f892a78cf7daad238d2e08d
mateo <matteoramirezz23@gmail.com>
2023-12-01 09:17:51 -0300

    feat(crud-products): importando la interfaz de Products

--- src/app/modules/admin/services/crud-products.service.ts
+++ src/app/modules/admin/services/crud-products.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
+import { Products } from 'src/app/models/products.model';
 
 @Injectable({
   providedIn: 'root'
