d9fc60a21746473a3cf6149b04214c2cd42ba684
mateo <matteoramirezz23@gmail.com>
2023-12-01 09:41:33 -0300

    feat(delete-product-button): importando input

--- src/app/modules/admin/products-components/delete-product-button/delete-product-button.component.ts
+++ src/app/modules/admin/products-components/delete-product-button/delete-product-button.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CrudProductsService } from '../../services/crud-products.service';
 import { Router } from '@angular/router';
 
