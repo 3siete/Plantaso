c344c618924f62b94212dbc35984b660131857c7
mateo <matteoramirezz23@gmail.com>
2023-12-01 09:44:06 -0300

    feat(card-shop): declarando cardShopData

--- src/app/shared/components/card-shop/card-shop.component.ts
+++ src/app/shared/components/card-shop/card-shop.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CardShop } from 'src/app/models/card-shop';
 
 @Component({
   selector: 'app-card-shop',
@@ -6,6 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./card-shop.component.css']
 })
 export class CardShopComponent {
+  
+  @Input() cardShopData: CardShop;
+  @Output() seeMoreButton: EventEmitter<string> = new EventEmitter<string>();
   constructor(){}
 
 }
