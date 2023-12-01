2b6a63980b25cc5a8e87c0736662dc5ca4ebc8e9
mateo <matteoramirezz23@gmail.com>
2023-12-01 09:39:18 -0300

    feat(delete-product-button): eliminando archivos innecesarios

--- src/app/modules/admin/products-components/delete-product-button/delete-product-button.component.ts
+++ src/app/modules/admin/products-components/delete-product-button/delete-product-button.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-delete-product-button',
-  templateUrl: './delete-product-button.component.html',
-  styleUrls: ['./delete-product-button.component.css']
+  template: './delete-product-button.component.html',
 })
 export class DeleteProductButtonComponent {
 
