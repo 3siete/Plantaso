dff2f4954f6f318470c18cdb18af2ae195774113
mateo <matteoramirezz23@gmail.com>
2023-12-01 09:53:04 -0300

    feat(articles-page): declarando CardShop

--- src/app/modules/products/pages/products-page/products-page.component.ts
+++ src/app/modules/products/pages/products-page/products-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CardShop } from 'src/app/models/card-shop';
 
 @Component({
   selector: 'app-products-page',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./products-page.component.css']
 })
 export class ProductsPageComponent {
-
+cardShop:CardShop
 }
