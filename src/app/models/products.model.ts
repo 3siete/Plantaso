8a1f6d8756c2638fe953a056a9ad7e074fa82a08
mateo <matteoramirezz23@gmail.com>
2023-12-01 09:05:01 -0300

    feat(products.model): terminando interfaz

--- src/app/models/products.model.ts
+++ src/app/models/products.model.ts
@@ -1,3 +1,14 @@
 export interface Products{
-    
+    productId:''
+    imgURL:'',
+    alt:'',
+    title:'',
+    slug:'',
+    price:'',
+    quotas:'',
+    info:'',
+    description:'',
+    quantity:'',
+    totalStock:'',
+    carrusel:''
 }
