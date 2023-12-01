e17b0a59bd243b6d3b64bc222bf9cc4af5738b21
mateo <matteoramirezz23@gmail.com>
2023-12-01 07:23:39 -0300

    feat(delete-article-button): eliminando archivos innecesarios

--- src/app/modules/admin/articles-components/delete-article-button/delete-article-button.component.ts
+++ src/app/modules/admin/articles-components/delete-article-button/delete-article-button.component.ts
@@ -4,7 +4,6 @@ import { CrudArticlesService } from '../../services/crud-articles.service';
 @Component({
   selector: 'app-delete-article-button',
   template: `<i class="fa-solid fa-trash fa-2xl" style="color: #ffffff;"></i>`,
-  styleUrls: ['./delete-article-button.component.css']
 })
 export class DeleteArticleButtonComponent {
   @Input() articleId!: string;
