34e8bba83963da58e36a16b0eb823a2b69fa8d74
mateo <matteoramirezz23@gmail.com>
2023-12-01 09:29:54 -0300

    feat(create-product-button): importando validadores

--- src/app/modules/admin/products-components/create-product-button/create-product-button.component.ts
+++ src/app/modules/admin/products-components/create-product-button/create-product-button.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CrudProductsService } from '../../services/crud-products.service';
 
 @Component({
