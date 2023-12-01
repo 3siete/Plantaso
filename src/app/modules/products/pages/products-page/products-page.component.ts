6525323725c42c2d2c7124ddf16c5d1488185bcb
mateo <matteoramirezz23@gmail.com>
2023-12-01 09:59:34 -0300

    feat(articles-page): terminando funcion seeMore

--- src/app/modules/products/pages/products-page/products-page.component.ts
+++ src/app/modules/products/pages/products-page/products-page.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CardShop } from 'src/app/models/card-shop';
 import { CrudArticlesService } from 'src/app/modules/admin/services/crud-articles.service';
+import { CrudProductsService } from 'src/app/modules/admin/services/crud-products.service';
 import { MappingService } from 'src/app/modules/articles/services/mapping.service';
 
 @Component({
@@ -13,7 +14,33 @@ export class ProductsPageComponent {
 cardShop:CardShop[] = [];
 constructor(
   private router: Router,
-  private crudService: CrudArticlesService,
+  private crudService: CrudProductsService,
   private slugMap: MappingService
-){}
+){
+  this.crudService.getCardShops().subscribe(
+    (cardShop:CardShop[])=>{
+      this.cardShop = cardShop
+    },
+    error=>{
+      console.error(error)
+    }
+  )
+}
+seeMore(productId:string){
+  this.crudService.getSlugForProduct(productId).subscribe(
+
+    slug=>{
+      if(slug){
+        this.router.navigate(['/producto',slug])
+      }else {
+        console.warn(`No se encontró slug para el productId: ${productId}`);
+        // Manejar según tus necesidades (por ejemplo, mostrar un mensaje de error)
+      }
+    },
+    error => {
+      console.error('Error al obtener el slug:', error);
+      // Manejar según tus necesidades (por ejemplo, mostrar un mensaje de error)
+    }
+  )
+}
 }
