444522c00b9f10a4741249bf63716c6b45bb380c
mateo <matteoramirezz23@gmail.com>
2023-12-01 09:40:40 -0300

    feat(delete-product-button): completando metodo delete

--- src/app/modules/admin/products-components/delete-product-button/delete-product-button.component.ts
+++ src/app/modules/admin/products-components/delete-product-button/delete-product-button.component.ts
@@ -5,5 +5,20 @@ import { Component } from '@angular/core';
   template: `<i class="fa-solid fa-trash fa-2xl" style="color: #ffffff;" (click)="confirmDelete()"></i>`,
 })
 export class DeleteProductButtonComponent {
+  @Input() productId!: string;
 
+  constructor(private crudService: CrudProductsService, private router: Router) {}
+
+  confirmDelete(): void {
+    const confirmation = window.confirm('¿Estás seguro de que quieres eliminar este producto?');
+    if (confirmation) {
+      this.crudService.deleteProduct(this.productId).subscribe(
+        () => {
+          console.log('Producto eliminado con éxito');
+          this.router.navigate(['producto']);
+        },
+        error => console.error('Error al eliminar el producto', error)
+      );
+    }
+  }
 }
